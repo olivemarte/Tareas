a3d7f87b7fcc0e96690564c7b62ec11984d65060
olivermarte <olivermarte15@gmail.com>
2023-10-21 19:52:11 -0400

    0.2

--- sueldo 13.py
+++ sueldo 13.py
@@ -4,5 +4,5 @@ i=1
 for i in range (12):
   sueldo= int(input("Digite un sueldo: "))
   resultado=resultado+sueldo
-print("El sueldo 13 es igual a",resultado)
+print("El sueldo 13 es igual as",resultado)
 
